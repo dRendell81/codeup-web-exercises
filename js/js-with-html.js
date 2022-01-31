77407382077fcf1dffd31ddd6b6c2b9e1557d0df
Deon Rendell <deonrendell@gmail.com>
2022-01-31 15:04:09 -0600

    add js-with-html file and js directory

---
+++ js/js-with-html.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// alert("Hello, World");
+// alert("This is my Second Alert");
+// confirm("Are you sure you want to leave this page?");
+// var userChoice = confirm("Are you sure you want to leave this page?");
+// console.log(userChoice);
+//
+// if(userChoice) {
+//     alert("Wise choice!");
+// }
+// alert("Boooooo! Good bye!");
+// }
+
+
+var currentSpeed = prompt("How fast are you driving now?");
+console.log(currentSpeed);
+
+var plannedSpeedIncrease = prompt("how many more mph do you want to go");
+
+console.log("Your planned speed is " + currentSpeed + plannedSpeedIncrease);
