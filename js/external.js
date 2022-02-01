699ba0b77290838e9e2e34c5f8582499f12d0028
Deon Rendell <deonrendell@gmail.com>
2022-02-01 15:55:23 -0600

    Was conducting Codeup webexercises

---
+++ js/external.js
@@ -0,0 +1,42 @@
+// "use strict";
+// console.log('Hello from External JavaScript!');
+// alert("Welcome to my website");
+// var blue = prompt("What's your favorite color?");
+//
+//
+// alert("Great! " + blue + " my favorite color too!")
+
+// var littleMermaid
+// var brotherBear
+// var Hercules
+// var littleMermaidRentalDays = 3
+// var brotherBearRentalDays = 5
+// var herculesRentalDays = 1
+// var
+
+// var littleMermaid = Number(prompt("How many days did you rent Little Mermaid for?"));
+//
+// var brotherBear = Number(prompt("How many days did you rent Brother Bear for?"));
+//
+// var Hercules = Number(prompt("How many days did you rent Hercules for?"));
+//
+// var total = littleMermaid + brotherBear + Hercules;
+//
+// alert("Your total cost for the rentals is " + (total * 3));
+
+//
+var googleHours = Number(prompt("How many hours did you work for Google?"))
+
+var googlePay = 400
+
+var amazonHours = Number(prompt(" How many hours did you work for Amazon?"));
+
+var amazonPay = 380
+
+var facebookHours = Number(prompt(" How many hours did you work for Facebook?"));
+
+var facebookPay = 350
+
+var totalPay = (googleHours * googlePay) + (amazonHours * amazonPay) + (facebookHours * facebookPay);
+
+alert("You will be paid " + totalPay + " this week")
