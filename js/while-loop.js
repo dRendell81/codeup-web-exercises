4456c09d7d3f8ec33c340c24e329cd54a566310f
Deon Rendell <deonrendell@gmail.com>
2022-02-07 16:31:50 -0600

    state of deon rendell prior to merging

--- js/while-loop.js
+++ js/while-loop.js
@@ -16,16 +16,41 @@
 //
 // var number = Math.floor(Math.random() *6) +1
 
-var allCones = Math.floor(Math.random() * 50) + 50;
- randomNum = Math.floor(Math.random() * 5) + 1;
+// var allCones = Math.floor(Math.random() * 50) + 50;
+//  randomNum = Math.floor(Math.random() * 5) + 1;
+//
+// do {
+//     var randomNum = Math.floor(Math.random() * 5) + 1;
+//     allCones = allCones - randomNum;
+//     console.log(allCones + " ran out of numbers " + randomNum)
+// }while (allCones >0);
+// if (allCones  === 5) {
+//     console.log("There are enough cones");
+// }else if ("Cant sell anymore");
+// console.log("All out of cones");
 
-do {
-    var randomNum = Math.floor(Math.random() * 5) + 1;
-    allCones = allCones - randomNum;
-    console.log(allCones + " ran out of numbers " + randomNum)
-}while (allCones >0);
-if (allCones  === 5) {
-    console.log("There are enough cones");
-}else if ("Cant sell anymore");
-console.log("All out of cones");
+// var count = 100
+// while (count >= 0) {
+//     console.log(count);
+//     count = count -1;
+// }
 
+// for(var count = 100; count >=0; = count -1 ){
+//     console.log(count);
+// }
+//
+// //variable called i for iterator
+//
+// for (var i = 100; i >= 0; i--1){
+//     console.log(i);
+// }
+
+// for (var i = 0; i <=100; i++){
+//     console.log(i);
+// }
+
+for (i = 0; i < 100; i++){
+    console.log(i);
+}
+
+console.log("After the loop ends, i is " + i);
