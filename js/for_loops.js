f7a7ab3615694f40f3d67a46d336d0f19883345f
Deon Rendell <deonrendell@gmail.com>
2022-02-08 11:10:39 -0600

    Finished working on loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,7 +1,40 @@
 "use strict"
 
-// for(var count = 100; count >=5; = count - 5){
+// .html file is while-loop.html
+//
+// for(var count = 100; count >=5; count = count - 5){
 //     console.log(count);
 // }
+//
+// alert("yo");
+//
+// var number = Math.floor(Math.random() * (200 -20 + 1) + 20)
+//
+// for (var i = 1; i <=10; i++){
+//     var randomNumber = Math.floor(Math.random() * (200 -20 + 1) + 20)
+//     if (randomNumber % 2 === 0) {
+//         console.log(randomNumber + "is even")
+//     }else{
+//         console.log(randomNumber+ "is odd");
+//     }
+// }
+//
+// var showMultiplicationTable = 7;
+// for (var i = 1; 1 <=10; i++){
+//     var result = showMultiplicationTable * i;
+//     console.log(showMultiplicationTable + ' * ' + i ' = ' + result);
+// }
+//
+// for(var i =1; i <=9; i++) {
+//     console.log(i.toString().repeat(i));
+// }
 
-alert("yo");
+for (var number = 1; number < 10; number++) {
+    var count = 0;
+    var outputString = "";
+    while (count <number){
+        outputString = outputString + number.toString();
+        count = count + 1;
+    }
+    console.log(outputString);
+}
--- js/while-loop.js
+++ js/while-loop.js
@@ -5,7 +5,7 @@
 //     console.log(number);
 //     number = number * 2;
 // }
-
+//
 // var number = 2;
 // while(number <= 65536) {
 //     console.log(number);
@@ -15,7 +15,7 @@
 // // Do-while
 //
 // var number = Math.floor(Math.random() *6) +1
-
+//
 // var allCones = Math.floor(Math.random() * 50) + 50;
 //  randomNum = Math.floor(Math.random() * 5) + 1;
 //
@@ -28,13 +28,13 @@
 //     console.log("There are enough cones");
 // }else if ("Cant sell anymore");
 // console.log("All out of cones");
-
+//
 // var count = 100
 // while (count >= 0) {
 //     console.log(count);
 //     count = count -1;
 // }
-
+//
 // for(var count = 100; count >=0; = count -1 ){
 //     console.log(count);
 // }
@@ -44,13 +44,13 @@
 // for (var i = 100; i >= 0; i--1){
 //     console.log(i);
 // }
-
+//
 // for (var i = 0; i <=100; i++){
 //     console.log(i);
 // }
-
-for (i = 0; i < 100; i++){
-    console.log(i);
-}
-
-console.log("After the loop ends, i is " + i);
+//
+// for (i = 0; i < 100; i++){
+//     console.log(i);
+// }
+//
+// console.log("After the loop ends, i is " + i);
