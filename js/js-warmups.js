08da34f3ecc8328fd19bdb190c906ef16b1ba233
Deon Rendell <deonrendell@gmail.com>
2022-02-08 09:37:07 -0600

    Added warmup exercise

--- js/js-warmups.js
+++ js/js-warmups.js
@@ -1,13 +1,45 @@
 "use strict"
 
-function warmUps(x){
-    return "I have no idea what you just said";
+// function warmUps(x){
+//     return "I have no idea what you just said";
+// }
+// console.log(warmUps("obey me"));
+//
+// function warmUps2(num){
+//     return num * 2
+//
+// }
+//
+// console.log(warmUps2();
+
+function numberPlusItSelf(number) {
+    return number + number;
 }
-console.log(warmUps("obey me"));
 
-function warmUps2(num){
-    return num * 2
+function sumofDoubles ("num1, num2") {
+    return ("num1 * 2) + (num2 * 2");
 
 }
 
-console.log(warmUps2();
+function squareANumber (number) {
+    return (number * number);
+}
+
+
+function stringLength(string) {
+    return string.lenth;
+}
+
+function sumOfStringLengths(string1, string2) {
+    return string1.length + string2.length;
+}
+
+function oddOrEven(number) {
+    if (number % 2 === 0) {
+        return "even";
+
+
+    }else}
+    return "odd"
+
+}
