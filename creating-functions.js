536af5be1087a3135cd28713daf48bf587725830
Deon Rendell <deonrendell@gmail.com>
2022-03-01 15:54:55 -0600

    adding bom exercise

---
+++ creating-functions.js
@@ -0,0 +1,22 @@
+// Steps to Create Your Own Function Writing Problems...
+//
+// 1. Choose the input (how many inputs? what data types?)
+// - number, string, boolean, array, object (less common: null, undefined)
+//
+// 2. Choose an output data type
+// - number, string, boolean, array, object (less common: null, undefined)
+//
+// 3. Consider what the function will do to change the input into the output
+// - will it combine inputs together to form the output?
+//     - will it return some smaller portion of the input?
+//     - will it convert from one data type to another?
+//
+//     4. Choose the most accurate name for the function
+//
+// 5. Write the function
+//
+// Tips...
+//
+// - start very simply
+// - try to vary existing function writing problems first (from 101/assessments/online resources/etc.)
+// - share and collaborate with others in the cohort
